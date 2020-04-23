06cfad8718ceb89c6aba8aa0f1f3be70049aa38a
Jake Kravas <jakekravas7@gmail.com>
2020-04-22 18:16:56 -0700

    removed jquery

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,16 +1,20 @@
 import React, {useState} from 'react';
 import axios from "axios";
-import $ from "jquery";
 import SearchResults from "./SearchResults";
 
 const Search = () => {
   const [loading, setLoading] = useState(0);
   const [books, setBooks] = useState();
+  const [searchVal, setSearchVal] = useState("");
+
+  const handleSearchVal = e => {
+    setSearchVal(e.target.value);
+  }
 
   const searchForBooks = async (e) => {
     e.preventDefault();
     setLoading(1); //Signifies that loading has began
-    const res = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${$("#search").val()}`);
+    const res = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${searchVal}`);
     
     setBooks(res.data.items); //Setting books to whatever the API search returns
     setLoading(2); //Signifies that loading has finished
@@ -21,7 +25,7 @@ const Search = () => {
       <h3>Book Search</h3>
       <form>
         <div className="input-group">
-          <input type="text" placeholder="Book" autoComplete="off" className="form-control" id="search"/>
+          <input type="text" onChange={handleSearchVal} placeholder="Book" autoComplete="off" className="form-control" id="search"/>
           <div className="input-group-append">
             <button className="btn btn-outline-secondary" onClick={searchForBooks}>Search</button>
           </div>
